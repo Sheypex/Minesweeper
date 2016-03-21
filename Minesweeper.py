ac7d03bdc5bbe5f8b8810591416947a89362c1a0
Sheypex <Sheypex@gmail.com>
2016-03-21 18:40:12 +0100

    Adds Bunch-Class to get the get and setAttr functions working
    Also moves attributes of FieldElem to a new Bunch-Object; FieldElem's function adapted respectively.

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Standard(object):
     def __setitem__(self, key, value):
         setattr(self, key, value)
@@ -6,12 +9,32 @@ class Standard(object):
     def __getitem__(self, item):
         return getattr(self, item, None)
 
-    def getAttr(self, name):
-        return self[name]
+    # def getAttr(self, name):
+    #     return self[name]
+    #
+    # def setAttr(self, name, val):
+    #     self[name] = val
+    #     return self[name]
 
     def setAttr(self, name, val):
-        self[name] = val
-        return self[name]
+        return self.__setattr__(name, val)
+
+    def getAttr(self, name):
+        return self.__getattribute__(name)
+
+
+class Bunch(dict):
+    def __init__(self, **kw):
+        dict.__init__(self, kw)
+        self.__dict__.update(kw)
+
+    def __str__(self):
+        state = ["%s=%r" % (attribute, value)
+                 for (attribute, value)
+                 in self.__dict__.items()]
+        return '\n'.join(state)
+
+    __getattr__ = dict.__getitem__
 
 
 class Field(Standard):
@@ -24,12 +47,56 @@ class Field(Standard):
         for i in range(w):
             buf = []
             for j in range(h):
-                buf.append(FieldElem(self, Coordinate(j, i), None, None))
+                buf.append(FieldElem(self, Coordinate(j, i), "N", 0))
             self.__field.append(buf)
+        self.generateField()
+
+    def generateField(self, maxIter=400):
+        minesLeft = self.__mines
+        area = self.__w * self.__h
+        if not minesLeft < area:
+            print("Error #1")
+            return None
+        currL = 0
+        currT = 0
+        while minesLeft > 0 and maxIter >= 0:
+            rand = random.random()
+            quo = area / minesLeft
+            if rand <= quo:
+                self.getFieldElem(Coordinate(currL, currT)).setAttr("__fieldType", "M")
+                minesLeft -= 1
+            if currL < self.__w - 1:
+                currL += 1
+            elif currT < self.__h - 1:
+                currT += 1
+            elif not (currL < self.__w - 1) and currT < self.__h - 1:
+                currL = 0
+                currT += 1
+            elif currL < self.__w - 1 and not (currT < self.__h - 1):
+                currL += 1
+                currT = 0
+            else:
+                currL = 0
+                currT = 0
+            maxIter -= 1
+
+    def log(self):
+        for i in range(2 * self.__w + 3):
+            print("#", end="")
+        print("")
+        for i, v in enumerate(self.__field):
+            print("# ", end="")
+            for j, w in enumerate(v):
+                print(str(w.getAttr("__fieldType")) + " ", end="")
+            print("#")
+        for i in range(2 * self.__w + 3):
+            print("#", end="")
+        print("")
 
     def getFieldElem(self, coordinates):
         """
 
+        :type coordinates: Coordinate
         :rtype: FieldElem
         """
         if 0 <= coordinates.t <= self.__h - 1 and 0 <= coordinates.l <= self.__w - 1:
@@ -38,6 +105,10 @@ class Field(Standard):
             return None
 
     def setFieldElem(self, coordinates, val):
+        """
+
+        :type coordinates: Coordinate
+        """
         if 0 <= coordinates.t <= self.__h - 1 and 0 <= coordinates.l <= self.__w - 1:
             self.__field[coordinates.t][coordinates.l] = val
             return self.__field[coordinates.t][coordinates.l]
@@ -45,6 +116,10 @@ class Field(Standard):
             return None
 
     def setFieldElemProperty(self, coordinates, property, val, create=False):
+        """
+
+        :type coordinates: Coordinate
+        """
         if 0 <= coordinates.t <= self.__h - 1 and 0 <= coordinates.l <= self.__w - 1:
             if not create:
                 if hasattr(self.__field[coordinates.t][coordinates.l], property):
@@ -59,24 +134,40 @@ class Field(Standard):
                 return None
 
     def getNeighbour_Down(self, ownCoordinates):
+        """
+
+        :type ownCoordinates: Coordinate
+        """
         if ownCoordinates.t + 1 <= self.__h - 1:
             return self.__field[ownCoordinates.t + 1][ownCoordinates.l]
         else:
             return None
 
     def getNeighbour_Up(self, ownCoordinates):
+        """
+
+        :type ownCoordinates: Coordinate
+        """
         if ownCoordinates.t - 1 >= 0:
             return self.__field[ownCoordinates.t - 1][ownCoordinates.l]
         else:
             return None
 
     def getNeighbour_Left(self, ownCoordinates):
+        """
+
+        :type ownCoordinates: Coordinate
+        """
         if ownCoordinates.l - 1 >= 0:
             return self.__field[ownCoordinates.t][ownCoordinates.l - 1]
         else:
             return None
 
     def getNeighbour_Right(self, ownCoordinates):
+        """
+
+        :type ownCoordinates: Coordinate
+        """
         if ownCoordinates.l + 1 <= self.__w - 1:
             return self.__field[ownCoordinates.t][ownCoordinates.l + 1]
         else:
@@ -90,6 +181,11 @@ class Field(Standard):
         #   True: will opt for method="walk" if "warp" previously failed to return/find a fieldElem in the field
         #   False: will not do so and rather return None -> might be used to check if a fieldElem exists at a point \
         #          relative to a given one, not really useful though(?)
+        """
+
+        :type relCoordinates: Coordinate
+        :type ownCoordinates: Coordinate
+        """
         if method == "walk":
             selectedFieldElem = self.getFieldElem(ownCoordinates)
             if relCoordinates.l != 0:
@@ -151,6 +247,10 @@ class Field(Standard):
             return None
 
     def getAdjacent(self, coordinates, returnType="dict"):
+        """
+
+        :type coordinates: Coordinate
+        """
         adjacentFieldElems = None
         if returnType == "dict":
             adjacentFieldElems = {"u": self.getNeighbour(coordinates, Coordinate(0, -1), autoAdjustMethod=False),
@@ -187,29 +287,41 @@ class Coordinate(Standard):
 
 class FieldElem(Standard):
     def __init__(self, fieldObj: Field, coordinates: Coordinate, fieldElemType, value):
-        self.__fieldObj = fieldObj
-        self.__coordinates = coordinates
-        self.__fieldType = fieldElemType
-        self.__value = value
-        self.__hidden = True
+        # self.__fieldObj = fieldObj
+        # self.__coordinates = coordinates
+        # self.__fieldType = fieldElemType
+        # self.__value = value
+        # self.__hidden = True
+        self.__bunch = Bunch(__fieldObj=fieldObj, __coorinates=coordinates, __fieldType=fieldElemType, __value=value,
+                             __hidden=True)
 
-    def getCoordinates(self):
-        return self.__coordinates
+    def setAttr(self, name, val):
+        self.__bunch[name] = val
+
+    def getAttr(self, name):
+        return self.__bunch[name]
 
     def log(self, **kwargs):
-        self.__coordinates.log(**kwargs)
+        self.__bunch.__coordinates.log(**kwargs)
 
     def getNeighbour(self, relCoordinates=Coordinate()):
-        return self.__fieldObj.getNeighbour(self.__coordinates, relCoordinates)
+        """
+
+        :rtype: list of FieldElem
+        """
+        return self.__bunch.__fieldObj.getNeighbour(self.__bunch.__coordinates, relCoordinates)
 
     def getAdjacent(self, returnType):
-        return self.__fieldObj.getAdjacent(self.__coordinates, returnType=returnType)
+        """
+
+        :rtype: list of FieldElem
+        """
+        return self.__bunch.__fieldObj.getAdjacent(self.__bunch.__coordinates, returnType=returnType)
 
 
 def main():
     field = Field(16, 16, 40)
-    field.setFieldElemProperty(Coordinate(0, 0), "__hidden", 5, create=False)
-    print("done")
+    field.log()
 
 
 if __name__ == '__main__':
